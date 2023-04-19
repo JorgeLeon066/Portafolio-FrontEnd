dd3beddc6e9849cdaedd78639033936e3a0ff4b1
JorgeLeon066 <almendrasj315@gmail.com>
2023-04-19 04:05:43 -0300

    second commit

--- ProyectoIntegrador_AlmendrasJorgeLuisLeon/src/app/app.module.ts
+++ ProyectoIntegrador_AlmendrasJorgeLuisLeon/src/app/app.module.ts
@@ -3,9 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
-import { SocialComponent } from './components/social/social.component';
-import { BannerComponent } from './components/banner/banner.component';
+import { LogoAPComponent } from './components/header/logo-ap/logo-ap.component';
+import { SocialComponent } from './components/header/social/social.component';
+import { BannerComponent } from './components/header/banner/banner.component';
+import { AcercaDeComponent } from './components/main/acerca-de/acerca-de.component';
+import { MainComponent } from './components/main/main.component';
 
 @NgModule({
   declarations: [
@@ -13,7 +15,9 @@ import { BannerComponent } from './components/banner/banner.component';
     HeaderComponent,
     LogoAPComponent,
     SocialComponent,
-    BannerComponent
+    BannerComponent,
+    AcercaDeComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule
