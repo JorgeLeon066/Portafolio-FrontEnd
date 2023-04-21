da23e81560fd4f444cf3ff88fd74276d90341cfb
JorgeLeon066 <almendrasj315@gmail.com>
2023-04-21 08:57:20 -0300

    maqueta completa

---
+++ Proyecto Integrador/src/app/components/atributes/atributes.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-atributes',
+  templateUrl: './atributes.component.html',
+  styleUrls: ['./atributes.component.css']
+})
+export class AtributesComponent {
+
+}
